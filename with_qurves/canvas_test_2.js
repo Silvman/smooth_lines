820f16febd46223c7cc1b84e7e9f8c461453fcb2
Silvman <anoscillator@gmail.com>
2018-12-30 03:58:22 +0300

    unexpected effect :0

--- with_qurves/canvas_test_2.js
+++ with_qurves/canvas_test_2.js
@@ -4,7 +4,11 @@ let isDrawingMode = false;
 let ox = 0;
 let oy = 0;
 
-let line_weight = 1;
+let px = 0;
+let py = 0;
+
+let line_weight = 5;
+let last_dd = 20;
 
 let can_catch_pointer = true;
 
@@ -42,11 +46,13 @@ function draw() {
     canvas.addEventListener('mouseup', (e) => {
         isDrawingMode = false;
 
+        px = 0;
+        py = 0;
+
         let x = e.pageX - x_offset;
         let y = e.pageY - y_offset;
 
         ctx.beginPath();
-        ctx.lineWidth = line_weight;
         ctx.lineCap = 'round';
         ctx.fillStyle = 'rgb(0,0,0)';
         ctx.moveTo(ox, oy);
@@ -66,13 +72,65 @@ function draw() {
                 const nx = ox + dd  * (x - ox) / d;
                 const ny = oy + dd  * (y - oy) / d;
 
+                if (px === 0 && py === 0) {
+                    px = (nx + ox) / 2;
+                    py = (ny + oy) / 2;
+                } else {
+                    let k = (oy - py) / (ox - px);
+                    let dist = ((nx - ox) * (nx - ox) + (ny - oy) * (ny - oy)) / 16;
+
+                    if (Math.abs(k) > 1) {
+                        k = 1 / k;
+
+                        let a = 1;
+                        let b = -2 * oy;
+
+                        let dis = 4 * dist / (1 + k * k);
+
+                        let t1 = (-b + Math.sqrt(dis)) / 2 * a;
+                        let t2 = (-b - Math.sqrt(dis)) / 2 * a;
+
+                        let test;
+
+                        if (ny >= oy) {
+                            py = t1;
+                        } else {
+                            py = t2;
+                        }
+
+                        px = (py - oy) * k + ox;
+                    } else {
+                        let a = 1;
+                        let b = -2 * ox;
+
+                        let dis = 4 * dist / (1 + k * k);
+
+                        let t1 = (-b + Math.sqrt(dis)) / 2 * a;
+                        let t2 = (-b - Math.sqrt(dis)) / 2 * a;
+
+                        let test;
+                        if (nx >= ox) {
+                            px = t1;
+                        } else {
+                            px = t2;
+                        }
+
+                        py = (px - ox) * k + oy;
+                    }
+                }
+
+                if (Number.isNaN(px) || Number.isNaN(py)) {
+                    console.log(new Error("Nan"));
+                    px = (nx + ox) / 2;
+                    py = (ny + oy) / 2;
+                }
 
                 ctx.beginPath();
                 ctx.lineWidth = line_weight;
                 ctx.lineCap = 'round';
                 ctx.fillStyle = 'rgb(0,0,0)';
-                ctx.moveTo(ox, oy);
-                ctx.lineTo(nx, ny);
+                ctx.moveTo(ox,oy);
+                ctx.quadraticCurveTo(px, py, nx, ny);
                 ctx.stroke();
 
                 ox = nx;
